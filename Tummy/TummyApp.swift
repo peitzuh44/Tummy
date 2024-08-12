a43ebdd9a627d77e23511a2a532e0f9f2d75dd78
Pei <peitzuh44@berkeley.edu>
2024-08-12 17:18:23 +0800

    Initial Commit

---
+++ Tummy/TummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TummyApp.swift
+//  Tummy
+//
+//  Created by Pei-Tzu Huang on 2024/8/12.
+//
+
+import SwiftUI
+
+@main
+struct TummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
