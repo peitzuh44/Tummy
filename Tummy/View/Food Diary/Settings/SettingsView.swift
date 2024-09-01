e03ba5588f6b5e6f63227f6096c3becbbf17ced9
Pei <peitzuh44@berkeley.edu>
2024-09-01 22:33:57 +0800

    Implement signout  and delete account

--- Tummy/View/Food Diary/Settings/SettingsView.swift
+++ Tummy/View/Food Diary/Settings/SettingsView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @ObservedObject var authManager: AuthManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button(action: authManager.signOut) {
+                Text("Sign Out")
+                    .foregroundColor(.red)
+            }
+            .padding()
+            
+            Button(action: authManager.deleteAccount) {
+                Text("Delete Account")
+                    .foregroundColor(.red)
+            }
+        }
+        .padding()
     }
+    
 }
 
-#Preview {
-    SettingsView()
-}
+
